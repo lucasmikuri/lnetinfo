5ee4ffa7cfebb9c407b86a4a164c5d7b0dd858cf
Lucas L. Mikuri <lucas_mikury@hotmail.com>
2023-11-01 00:51:12 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const result = await database.query("SELECT current_database();");
   console.log(result.rows);
   response.status(200).json({ chave: "Alunos do curso sao acima da media" });
 }
