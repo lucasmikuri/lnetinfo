ba26c5dd99468641e39f04f63075bdef2729ba67
Lucas L. Mikuri <lucas_mikury@hotmail.com>
2023-12-22 13:33:17 +0000

    todos

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
+import { Query } from "pg";
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT current_database();");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Alunos do curso sao acima da media" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseConnMaxResult = await database.query("SHOW max_connections;");
+  const databaseConnMaxValue = databaseConnMaxResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseConnUsoResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseConnUsoValue = databaseConnUsoResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseConnMaxValue),
+        uso_connections: databaseConnUsoValue,
+        opened_connections: databaseConnUsoValue,
+      },
+    },
+  });
 }
 
 export default status;
