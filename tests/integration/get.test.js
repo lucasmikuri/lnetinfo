12cab5990f5f297cc0d8bc31428d19bfc7235be7
Lucas L. Mikuri <lucas_mikury@hotmail.com>
2024-02-10 02:29:23 +0000

    aaa

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -8,7 +8,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAT = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAT);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.version).toEqual("16.1");
+  expect(responseBody.dependencies.database.max_connections).toEqual(112);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
